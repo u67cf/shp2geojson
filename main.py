ace45284ae7f71c83be3fc96a1ca76291590ee6c
u8b19 <bochainwu@gmail.com>
2025-04-01 12:32:32 +0800

    Add initial shapefile and GeoJSON conversion functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from shp2geojson!")
+import geopandas as gpd
 
+def main():
+    gdf = gpd.read_file("TOWN_MOI_1131028.shx")
+
+    gdf.to_file("TOWN_MOI_1131028.geojson", driver="GeoJSON")
 
 if __name__ == "__main__":
     main()
